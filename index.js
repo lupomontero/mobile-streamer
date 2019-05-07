e73d6797f1c84e4897d00dc467abb5751a7d1a6c
Lupo Montero <lupomontero@gmail.com>
2019-05-06 21:27:16 -0500

    Updates

--- index.js
+++ index.js
@@ -1,26 +1,102 @@
+const root = document.getElementById('root');
 const video = document.createElement('video');
-const startButton = document.createElement('button');
-const peerConnection = new RTCPeerConnection();
+const broadcastButton = document.createElement('button');
+const poolList = document.createElement('ul');
 
 
-startButton.innerHTML = 'Start';
+const config = {
+  iceServers: [{ urls: ['stun:stun.1.google.com:19302'] }],
+};
 
 
-document.body.appendChild(video);
-document.body.appendChild(startButton);
+const ws = new WebSocket('ws://192.168.1.2:8080/');
+let pool = [];
 
 
-startButton.addEventListener('click', () => {
+broadcastButton.innerHTML = 'Start';
+
+
+root.appendChild(video);
+root.appendChild(broadcastButton);
+root.appendChild(poolList);
+
+
+const updatePool = (newPool) => {
+  pool = newPool;
+  poolList.innerHTML = '';
+  pool.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerHTML = item.displayName;
+    poolList.appendChild(li);
+  });
+};
+
+
+broadcastButton.addEventListener('click', () => {
+  const displayName = prompt('Display name:');
+
   navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   })
     .then((stream) => {
-      alert('Got a stream!');
       video.srcObject = stream;
       video.play();
+
+      const peerConnection = new RTCPeerConnection(config);
+      peerConnection.addStream(stream);
+      peerConnection.createOffer()
+        .then(sdp => peerConnection.setLocalDescription(sdp))
+        .then(() => ws.send(JSON.stringify({
+          type: 'peerConnectionOffer',
+          displayName,
+          sessionDescription: peerConnection.localDescription,
+        })));
+
+      ws.addEventListener('message', (e) => {
+        const parsed = JSON.parse(e.data);
+        if (parsed.type === 'peerConnectionAnswer') {
+          peerConnection.setRemoteDescription(parsed.sessionDescription);
+        }
+      });
     })
     .catch((err) => {
       alert(err.message || err);
     });
+});
+
+
+
+poolList.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'LI') {
+    return;
+  }
+
+  const offer = pool.find(item => item.displayName === e.target.innerHTML);
+  const peerConnection = new RTCPeerConnection(config);
+
+  console.log('offer.sessionDescription', offer.sessionDescription);
+
+  peerConnection.setRemoteDescription(offer.sessionDescription)
+    .then(() => peerConnection.createAnswer())
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+      ws.send(JSON.stringify({
+        type: 'peerConnectionAnswer',
+        sessionDescription: peerConnection.localDescription,
+      }));
+    });
+
+  peerConnection.addEventListener('addStream', (event) => {
+    console.log('stream added!', event.stream);
+  });
+});
+
+
+ws.addEventListener('message', (e) => {
+  const parsed = JSON.parse(e.data);
+  console.log('ws::message', parsed);
+  if (parsed.type === 'peerConnectionPoolUpdate') {
+    updatePool(parsed.pool);
+  }
 });
